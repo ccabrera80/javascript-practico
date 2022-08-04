f9e39ef916a355a8471e60d55d34177519f6394f
ccabrera80 <human_carlosc@hotmail.com>
2022-08-03 19:01:59 -0500

    Pimera actualizacion

--- main.js
+++ main.js
@@ -1,1 +1,109 @@
-console.log('hello, world');
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const shopingCardContainer = document.querySelector('#shopingCardContainer');
+const cardsContainer = document.querySelector('.cards-container');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleDesktopMenu() {
+  const isAsideClosed = shopingCardContainer.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    shopingCardContainer.classList.add('inactive');
+  }
+  
+  desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  const isAsideClosed = shopingCardContainer.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    shopingCardContainer.classList.add('inactive'); 
+  }
+  
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive'); 
+  }
+  
+  shopingCardContainer.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+  name: 'Jersey The Faster 01',
+  price: 32,
+  image: './imagenes/THE FASTER 01.jpg',
+});
+
+productList.push({
+  name: 'Jersey The Faster 02',
+  price: 32,
+  image: './imagenes/THE FASTER 02.jpg',
+});
+
+productList.push({
+  name: 'Jersey The Faster 03',
+  price: 32,
+  image: './imagenes/THE FASTER 03.jpg',
+});
+
+productList.push({
+  name: 'Jersey The Faster 04',
+  price: 32,
+  image: './imagenes/THE FASTER 04.jpg',
+});
+
+
+function renderProductos (arr) {
+
+  for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    const ProductImg = document.createElement('img');
+    ProductImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+  
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(ProductImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+    
+  }
+}
+
+renderProductos(productList);
